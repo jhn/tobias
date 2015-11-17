d75ad313cc8c89727b8efed10b786e94120d6d0e
Johan Mena <johan.menac@gmail.com>
2015-11-17 18:51:25 -0500

    Add skeleton compojure routes

--- domain/project.clj
+++ domain/project.clj
@@ -1,6 +1,15 @@
 (defproject domain "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "CV + RTB"
+  :url "http://example.com/OMGLOL"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [ring/ring-defaults "0.1.5"]
+                 [ring/ring-json "0.4.0"]
+                 [compojure "1.4.0"]]
+  :plugins [[lein-ring "0.9.7"]]
+  :ring {:handler domain.core/app
+         :nrepl {:start? true
+                 :port 3001}}
+  :aot [domain.core])
--- domain/src/domain/core.clj
+++ domain/src/domain/core.clj
@@ -1,6 +1,17 @@
-(ns domain.core)
+(ns domain.core
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
+            [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
+            [ring.util.response :refer [response]])
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defroutes app-routes
+  (GET "/status" [] "OMG HI!")
+  (route/not-found "Not Found"))
+
+(def app
+  (-> app-routes
+      (wrap-defaults api-defaults)
+      (wrap-json-body {:keywords? true})
+      (wrap-json-response)))
