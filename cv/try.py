c783907423b5b95b46321645c25baf6e0ef687cd
ac3680 <ac3680@columbia.edu>
2015-11-29 03:49:03 -0500

    Work on forwarding body and multithreading
    Work on forwarding body and multithreading

--- cv/try.py
+++ cv/try.py
@@ -1,11 +1,17 @@
 #### Python 2.7 
-import pdb
+#import pdb
 import httplib, urllib, base64, json
+import os,http
+import sys
 body = {"URL": "http://www.burgermedia.nl/media/k2/items/cache/9911ecbea07a30e7c89fdadbe8a058e8_XL.jpg" } 
+print body
+print (open('1.jpg', "rb").read())[:50]
+
+#sys.exit()
 
 headers = {
-    'Content-Type': 'application/json',
-    'Ocp-Apim-Subscription-Key': '6e87c925b14b487fb7d38cd1eb4358c7',
+    'Content-Type': 'application/octet-stream',
+    'Ocp-Apim-Subscription-Key': '-------------------------------------------------------------------------------------------',
 } 
 
 params = urllib.urlencode({
@@ -16,19 +22,15 @@ params = urllib.urlencode({
     'analyzesHeadPose': 'false',
 })
 
-#params = 'faceRectangles'
-
 try:
     conn = httplib.HTTPSConnection('api.projectoxford.ai')
-    conn.request("POST", "/face/v0/detections?%s" % params, json.dumps(body) , headers)
+    conn.request("POST", "/face/v0/detections?%s" % params, open('1.jpg', "rb").read(), headers) #json.dumps(body) #open('1.jpg', "rb").read()
     response = conn.getresponse()
     data = response.read()
-    #print(data)
-    jdata = json.loads(data)
-    for each in jdata:
-        print each['attributes']
+    print data
+    #jdata = json.loads(data)
+    #for each in jdata:
+    #    print each['attributes']
     conn.close()
 except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
-    
-    
+    print("Errno")
