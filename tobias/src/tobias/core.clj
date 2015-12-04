6db3a1455899afcc9a15f0662b48d56ab2b7d160
Johan Mena <johan.menac@gmail.com>
2015-12-03 23:45:26 -0500

    Mimic response structure more closely in the simulation

--- tobias/src/tobias/core.clj
+++ tobias/src/tobias/core.clj
@@ -11,8 +11,6 @@
             [tobias.util :refer [timed load-config]])
   (:gen-class))
 
-(def ads->features (atom {}))
-
 (def features
   "A map that holds features and their possible values"
   {:ethnicity [:asian :black :hispanic :white]
@@ -20,38 +18,6 @@
    :gender    [:male :female]
    :age       [:young :mid :old]
    :weather   [:sunny :rainy]})
-
-(def ads->features-example
-  "An example of a map that holds each ad and its preferred features"
-  [{:id :one
-    :ethnicity :asian
-    :weather   :rainy
-    :location  :prime
-    :age       :mid
-    :gender    :male
-    :url       "http://www.zillowstatic.com/static/images/ad_gallery/chevy-300x250.jpg"}
-   {:id        :two
-    :age       :mid
-    :gender    :female
-    :location  :average
-    :url       "http://www.fashionadexplorer.com/l-yE0JkxpIlKdLO9ot.jpg"}
-   {:id        :three
-    :weather   :sunny
-    :gender    :male
-    :age       :mid
-    :url       "https://webtoolfeed.files.wordpress.com/2012/04/4bd863d620bf61.jpg"}
-   {:id        :four
-    :gender    :male
-    :age       :old
-    :location  :prime
-    :url       "http://files2.coloribus.com/files/adsarchive/part_944/9449805/file/life-insurance-deck-chair-small-69163.jpg"}])
-
-(def result-features-example
-  "An example of a resulting map obtained from Tobiaz' UI and CV"
-  {:location  :prime
-   :ethnicity :asian
-   :weather   :sunny
-   :gender    :male})
 
 (defn score-feature-set [feature-set result-set]
   "Returns an ad's feature set scored"
@@ -76,23 +42,24 @@
          (assoc {} :winner)
          (merge {:features resulting-features}))))
 
+(def ads (load-config (clojure.java.io/resource "ads.edn")))
+
 (defn run-auction [image env-features]
   (->> image
        (get-features)
        (first) ; select only one feature (possibly the one with highest confidence), should be more clever
        (merge env-features)
-       (get-winning-ad ads->features-example)))
+       (get-winning-ad ads)))
 
-(def ads (load-config (clojure.java.io/resource "ads.edn")))
-
-(defn run-simulation [image env-features] (rand-nth ads))
+(defn run-simulation [image]
+  {:winner (rand-nth ads)})
 
 (defroutes app-routes
   (GET "/" [] (content-type (resource-response "index.html" {:root "public"}) "text/html"))
   (POST "/auction/new"
         {{{image :tempfile} :file} :params} (response (run-auction image {:location :prime :weather :sunny})))
   (POST "/simulation/new"
-        {{{image :tempfile} :file} :params} (response (run-simulation image {:location :prime :weather :sunny})))
+        {{{image :tempfile} :file} :params} (response (run-simulation image)))
   (route/resources "/")
   (route/not-found "Not Found"))
 
