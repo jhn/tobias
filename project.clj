ed68a78cf8c730f13aea5f5157e1d209c13f2398
Johan Mena <johan.menac@gmail.com>
2015-12-08 15:39:19 -0500

    Use Clojure 1.8.0-RC3

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/OMGLOL"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0-RC3"]
                  [ring/ring-jetty-adapter "1.4.0"]
                  [ring/ring-defaults "0.1.5"]
                  [ring/ring-json "0.4.0"]
