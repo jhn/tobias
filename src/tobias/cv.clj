a3cfc6cac065854aa6f24bf742a316cc2d1ddbbf
Johan Mena <johan.menac@gmail.com>
2015-12-10 19:32:13 -0500

    Refactor cv

--- src/tobias/cv.clj
+++ src/tobias/cv.clj
@@ -30,57 +30,57 @@
 
 (defprotocol CvProvider
   "Encapsulates a CV provider"
-  (name       [this]        "The name for this provider")
-  (post-image [this image]  "Posts an image")
-  (normalize  [this result] "Normalizes a result"))
+  (name      [this]        "The name for this provider")
+  (features  [this image]  "Gets the features for an image")
+  (normalize [this result] "Normalizes a result"))
 
 (def microsoft
   (reify CvProvider
     (name [_] "Microsoft")
-
-    (post-image [_ image] (http/post (str "https://api.projectoxford.ai/face/v0/detections"
-                                          "?analyzesFaceLandmarks=false"
-                                          "&analyzesAge=true"
-                                          "&analyzesGender=true"
-                                          "&analyzesHeadPose=false")
-                                     {:headers {"Content-Type" "application/octet-stream"
-                                                "Ocp-Apim-Subscription-Key" (microsoft-creds :key)}
-                                      :body image}))
-
-    (normalize [_ result] (map (fn [r]
-                                 {:age    (age->sym (get-in r [:attributes :age]))
-                                  :gender (str->kw  (get-in r [:attributes :gender]))})
-                               result))))
+    (features [_ image]
+      (http/post (str "https://api.projectoxford.ai/face/v0/detections"
+                      "?analyzesFaceLandmarks=false"
+                      "&analyzesAge=true"
+                      "&analyzesGender=true"
+                      "&analyzesHeadPose=false")
+                 {:headers {"Content-Type" "application/octet-stream"
+                            "Ocp-Apim-Subscription-Key" (microsoft-creds :key)}
+                  :body image}))
+    (normalize [_ result]
+      (map (fn [r]
+             {:age    (age->sym (get-in r [:attributes :age]))
+              :gender (str->kw  (get-in r [:attributes :gender]))})
+           result))))
 
 (def sightcorp
   (reify CvProvider
     (name [_] "Sightcorp")
-
-    (post-image [_ image] (http/post "http://api.sightcorp.com/api/detect/"
-                                     {:multipart [{:name "img" :content image}
-                                                  {:name "app_key" :content (sightcorp-creds :app_key)}
-                                                  {:name "client_id" :content (sightcorp-creds :client_id)}]}))
-
-    (normalize [_ result] (map (fn [r]
-                                 {:age       (age->sym (get-in r [:age :value]))
-                                  :gender    (str->kw  (get-in r [:gender :value]))
-                                  :ethnicity (str->kw  (get-in r [:ethnicity :value]))
-                                  :clothing  (get r :clothingcolors [])})
-                               (:persons result)))))
+    (features [_ image]
+      (http/post "http://api.sightcorp.com/api/detect/"
+                 {:multipart [{:name "img" :content image}
+                              {:name "app_key" :content (sightcorp-creds :app_key)}
+                              {:name "client_id" :content (sightcorp-creds :client_id)}]}))
+    (normalize [_ result]
+      (map (fn [r]
+             {:age       (age->sym (get-in r [:age :value]))
+              :gender    (str->kw  (get-in r [:gender :value]))
+              :ethnicity (str->kw  (get-in r [:ethnicity :value]))
+              :clothing  (get r :clothingcolors [])})
+           (:persons result)))))
 
 (def faceplus
   (reify CvProvider
     (name [_] "Faceplus")
-
-    (post-image [_ image] (http/post "http://apius.faceplusplus.com/detection/detect"
-                                     {:multipart [{:name "img" :content image}
-                                                  {:name "api_key" :content (faceplus-creds :api_key)}
-                                                  {:name "api_secret" :content (faceplus-creds :api_secret)}]}))
-
-    (normalize [_ result] (map (fn [r]
-                                 {:age    (age->sym (get-in r [:attribute :age :value]))
-                                  :gender (str->kw  (get-in r [:attribute :gender :value]))})
-                               (:face result)))))
+    (features [_ image]
+      (http/post "http://apius.faceplusplus.com/detection/detect"
+                 {:multipart [{:name "img" :content image}
+                              {:name "api_key" :content (faceplus-creds :api_key)}
+                              {:name "api_secret" :content (faceplus-creds :api_secret)}]}))
+    (normalize [_ result]
+      (map (fn [r]
+             {:age    (age->sym (get-in r [:attribute :age :value]))
+              :gender (str->kw  (get-in r [:attribute :gender :value]))})
+           (:face result)))))
 
 (defn get-features
   "Returns a normalized response for the features in the image"
@@ -88,14 +88,13 @@
    (get-features image [sightcorp microsoft faceplus]))
 
   ([image providers]
-   (let [results (map (fn [provider]
-                        (thread
-                          (timed
-                            (name provider)
-                            (->> image
-                                 (post-image provider)
-                                 (extract-json-body)
-                                 (normalize provider)))))
-                      providers)
-         [first-result _] (alts!! results)]
-     first-result)))
+   (->> providers
+        (map (fn [provider]
+               (thread
+                 (timed (name provider)
+                        (->> image
+                             (features provider)
+                             (extract-json-body)
+                             (normalize provider))))))
+        (alts!!)     ; get the first result / chan pair
+        (first))))   ; only return the result
