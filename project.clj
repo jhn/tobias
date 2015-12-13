5dc530b1a3030768cc48bfca8e8acfa11cd7cb6b
Johan Mena <johan.menac@gmail.com>
2015-12-13 03:24:34 -0500

    Warn on reflection

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject tobias "0.1.0-SNAPSHOT"
   :description "Tobias CV"
-  :url "http://example.com/OMGLOL"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0-RC3"]
@@ -13,6 +12,7 @@
                  [org.clojure/data.json "0.2.6"]
                  [ring-cors "0.1.7"]]
   :plugins [[lein-ring "0.9.7"]]
+  :global-vars {*warn-on-reflection* true}
   :ring {:handler tobias.core/app
          :nrepl {:start? true :port 3001}}
   :profiles {:dev {:jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005"]}}
